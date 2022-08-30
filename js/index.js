fe8294cb312b873fd345cabb4a782e774888ff19
towsifcoder <tpatwary@ael-bd.com>
2022-08-30 14:36:00 +0600

    Design Modified

--- js/index.js
+++ js/index.js
@@ -34,3 +34,8 @@
 //     searchToggle.setAttribute("aria-label", "Open search");
 //   }
 // }
+
+$(document).ready(function () {
+    $('#dtBasicExample').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+});
